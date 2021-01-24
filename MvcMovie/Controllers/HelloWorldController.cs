e037bd45b3c265112ed7aba817e38b74ab35c9bd
QuangHuyZ <patrotothenus@gmail.com>
2021-01-24 20:23:05 +0700

    Z

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //----------------Add a model part
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        //-----------------Add a controller Part
+        /*
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        /*
+        public string Welcome(string name, int numTimes=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        */
+        /*
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+        */
+        //-----------------End Add a controller Part
+    }
+}
